3f05d4a38dddd53caf26ef0f344bac61084d5398
Nadezhda <catsnake@yandex.ru>
2021-09-13 15:30:07 +0300

    client-server api

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { requestTime, logger } from "./middlewares.js";
+import serverRoutes from "./routes/server.js";
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+console.log(app.get("views"));
+
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(requestTime);
+app.use(logger);
+
+app.use(serverRoutes);
+
+app.get("/", (req, res) => {
+	res.render("index", { title: "Main Page", active: "main" });
+});
+
+app.get("/features", (req, res) => {
+	res.render("features", { title: "Features Page", active: "features" });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server has been started on port ${PORT} ...`);
+});
